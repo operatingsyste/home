189d970b0bf935006a46ab4a1443bb8dbbd4561d
operatingsyste <134151646+operatingsyste@users.noreply.github.com>
2023-05-20 20:55:23 +0530

    Upload-1

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("Operating Syste version running : 0.0.1 <on_web_based>");
+
+console.log("Gdesk.info<netERROR>");
+
+function not_av(){
+  alert("Maintainence : This item is in build so please wait untill it build perfectly");
+}
